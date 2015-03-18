551bf479a35ea12cac9b6b3e82e67034e62b13be
Derek VerLee <dverlee@maddogtechnology.com>
2015-03-18 11:35:07 -0400

    Turn off whitespace-mode by default, turn off highlight line

---
+++ personal/preload/prelude-overrides.el
@@ -0,0 +1,6 @@
+;; turn off "whitespace mode" by default
+(setq prelude-whitespace nil)
+
+;; disable the "highlight current line" feature, it
+;;  doesn't work well with all themes
+(global-hl-line-mode nil)
