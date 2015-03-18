95b589e548a89ac9be613146ab0652555de9b2bb
Derek VerLee <dverlee@maddogtechnology.com>
2015-03-18 12:56:21 -0400

    Make a personal copy of the prelude-modules file

--- personal/preload/prelude-overrides.el
+++ personal/preload/prelude-overrides.el
@@ -4,3 +4,6 @@
 ;; disable the "highlight current line" feature, it
 ;;  doesn't work well with all themes
 (global-hl-line-mode nil)
+
+
+(setq-default prelude-modules-file (expand-file-name "my-prelude-modules.el" prelude-dir))
