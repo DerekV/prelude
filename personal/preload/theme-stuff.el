b3375ff767ca04fdde15621e5613ec2ad32263c0
Derek VerLee <dverlee@maddogtechnology.com>
2015-03-18 11:36:31 -0400

    Add themes generated from the Base16 project

---
+++ personal/preload/theme-stuff.el
@@ -0,0 +1,10 @@
+;;; theme_stuff --- Derek's theme personalizations
+
+;;; Commentary:
+
+;;; Code:
+
+;; Add a folder where I can load in themes myself
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+;;; theme_stuff ends here
