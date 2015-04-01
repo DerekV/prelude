522bbf6d8e1b5c45cdcd5e912e73fdef71c5ce0a
Derek VerLee <dverlee@maddogtechnology.com>
2015-04-01 11:36:30 -0400

    Use 2 space indent by default for markup in web-mode

---
+++ personal/web-mode.el
@@ -0,0 +1,7 @@
+;;; some settings related to web-mode
+;;; http://web-mode.org/
+
+(setq-default web-mode-markup-indent-offset 2)
+(setq-default web-mode-css-indent-offset 2)
+;;(setq-default web-mode-code-indent-offset tab-width)
+;;(setq-default web-mode-sql-indent-offset tab-width)
