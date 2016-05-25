bed9214ea04714a20e61d7b61442ce951247ef12
Derek VerLee <dverlee@maddogtechnology.com>
2016-05-25 17:02:28 -0400

    Enabling different modules

--- personal/preload/prelude-overrides.el
+++ personal/preload/prelude-overrides.el
@@ -5,5 +5,14 @@
 ;;  doesn't work well with all themes
 (global-hl-line-mode nil)
 
+;; smartparens piss me off, it doesn't work right
+(add-hook 'prelude-prog-mode-hook (lambda () (smartparens-mode -1)) t)
+
+;; guru-mode likes to tell me I shouldn't use arrow keys
+;;   but arrow keys are nicely placed on my keyboard...
+;;   so like, noted... meanwhile kindly fuck off
+(setq prelude-guru nil)
 
 (setq-default prelude-modules-file (expand-file-name "my-prelude-modules.el" prelude-dir))
+
+;;(setq whitespace-style (quote (spaces tabs newline)))
